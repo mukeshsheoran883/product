8a756f2ed26596417ce9189d75fa067d21bc357d
MukeshSheoran <mukeshsheoran883@gmail.com>
2023-11-19 14:45:42 +0530

    Product Screen commit

--- lib/screen/product_screen.dart
+++ lib/screen/product_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:product/model/products_model.dart';
+import 'package:product/service/provider_product.dart';
+import 'package:provider/provider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({
@@ -10,15 +13,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  void initState() {
+    ProviderProduct providerProduct =
+    Provider.of<ProviderProduct>(context, listen: false);
+    super.initState();
+    providerProduct.loadProducts();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Product'),
-      ),
-      body: const Column(
-        children: [],
-      ),
+        appBar: AppBar(
+          title: const Text('Product'),
+        ),
+        body: Consumer<ProviderProduct>(
+          builder: (BuildContext context, ProviderProduct providerProduct,
+              Widget? child) {
+            return ListView.builder(itemCount: providerProduct.productModel.length,
+              itemBuilder: (context, index) {
+              ProductsModel productsModel = providerProduct.productModel[index];
+              return InkWell(onTap: (){},
+                child: Column(
+                  children: [
+                    Image.network(productsModel.images!.first.toString()),
+                    ListTile(
+                      title: Text('Name : ${productsModel.title}'),
+                      subtitle:
+                      Text("Price : \$${productsModel.price.toString()}"),
+                    ),
+                  ],
+                ),
+              );
+            },);
+          },)
     );
   }
 }
--- lib/service/provider_product.dart
+++ lib/service/provider_product.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:product/model/products_model.dart';
+import 'package:product/service/api_service.dart';
 
 class ProviderProduct extends ChangeNotifier{
+  late ProductApiService productApiService;
+  List<ProductsModel> productModel = [];
+  Future<void> loadProducts() async {
+
+
+    productModel = await ProductApiService.getProductInfo();
+    notifyListeners();
+  }
 
 }
