1cffd027c3b49ecd10a390d2f627ea09f457e411
MukeshSheoran <mukeshsheoran883@gmail.com>
2023-11-19 14:07:43 +0530

    Api Service commit

---
+++ lib/service/api_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:product/model/products_model.dart';
+import 'package:product/model/products_response_list.dart';
+import 'package:product/util/api_end_point.dart';
+
+
+class ProductApiService {
+  static Future<List<ProductsModel>> getProductInfo() async {
+    String url = ApiEndPoint.product;
+    http.Response response = await http.get(
+      Uri.parse(url),
+    );
+    if (response.statusCode == 200) {
+      String body = response.body;
+      final data = jsonDecode(body);
+
+      ProductResponseList productResponseList =
+      ProductResponseList.fromJson(data);
+      return productResponseList.products;
+    } else {
+      throw 'Something went wrong';
+    }
+  }
+}
